b2c771c8b545bee270249226cc87ee47b557389b
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-12 00:38:54 +0300

    Finished setting up the event targets with regards to related button

---
+++ app.js
@@ -0,0 +1,20 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if(id) {
+        // Remove the active class from all buttons
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
+        // Hide other articles
+        articles.forEach((article) => {
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+});
